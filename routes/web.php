bf985c01c6ef5b8655b2a8903f502190822327c9
rosa <rosatiaragaluh@gmail.com>
2022-04-04 00:21:43 +0700

    feat: rendering data in views

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,16 @@ Route::get('/contact', function () {
 })->name('home.contact');
 
 Route::get('/blogposts/{id}', function($id) {
+    $posts = [
+        1 => [
+            'title' => 'Intro to Laravel',
+            'content' => 'This is a short intro to Laravel'
+        ],
+        2 => [
+            'title' => 'Intro to PHP',
+            'content' => 'This is a short intro to PHP'
+        ]
+        ];
     return 'blog post' . $id;
 })->name('blogposts.show');
 
