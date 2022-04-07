157174ae435a44fe9bb54634bdfcdcd57b9e7a90
rosa <rosatiaragaluh@gmail.com>
2022-04-08 00:35:26 +0700

    feat: update page

--- resources/views/blogposts/partials/form.blade.php
+++ resources/views/blogposts/partials/form.blade.php
@@ -1,10 +1,5 @@
-@extends('layout.app')
-@section('title', 'Create Blog Posts page')
-@section('content')
-  <h1 class="mt-4">Create a new blog post</h1>
-  <form action="{{ route('blogposts.store') }}" method="POST">
-  @csrf
-  @include ('blogposts.partials.form')
+<h1 class="mt-4">Create a new blog post</h1>
+  <form>
     <div class="mb-3">
       <label for="blogPostTitle" class="form-label">Blog Post Title</label>
       <input type="text" class="form-control" name="blogPostTitle" id="blogPostTitle" value="{{old('blogPostTitle')}}" aria-describedby="blogPostTitleHelp">
@@ -15,7 +10,7 @@
       <textarea name="blogPostContent" id="blogPostContent" class="form-control" aria-describedby="blogPostContentHelp">{{old('blogPostContent')}}</textarea>
       <small id="blogPostContent">Please insert the content of the Blog Post</small>
     </div>
-    <div class="mt-4">
+    <div class="mt-4 form-check">
       <div>
           <input type="checkbox" id="highlight" name="highlight" value="highlight">
           <label for="highlight">Highlight Blog Post</label>
