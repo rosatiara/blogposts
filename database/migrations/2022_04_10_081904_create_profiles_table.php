5bf9cb2fd6ae865e4b45ee78303190e3b33d53bd
rosa <rosatiaragaluh@gmail.com>
2022-04-10 15:24:05 +0700

    feat: add email & author_id to profile

--- database/migrations/2022_04_10_081904_create_profiles_table.php
+++ database/migrations/2022_04_10_081904_create_profiles_table.php
@@ -16,6 +16,8 @@ return new class extends Migration
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('email');
+            $table->foreignID('author_id')->unique()->constrained();
         });
     }
 
