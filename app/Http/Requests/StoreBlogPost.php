2055d0a0d3fb03ec829101644cb38c97d59d73d7
rosa <rosatiaragaluh@gmail.com>
2022-04-10 13:50:30 +0700

    add comments for explanation

--- app/Http/Requests/StoreBlogPost.php
+++ app/Http/Requests/StoreBlogPost.php
@@ -27,5 +27,12 @@ class StoreBlogPost extends FormRequest
             'blogPostTitle' => 'bail|required|min:5|max:100',
             'blogPostContent' => 'required|min:10'
         ];
+        /**
+         * bail = Stop running validation rules for the field after the first validation failure
+         * required = The field under validation must be present in the input data and not empty.
+         * min:value = minimum value on the blogPostTitle is <value>
+         * max:value = max value on the blogPostTitle is <value>
+         * blogPostContent = required, minimal 10 values, no maximum values.
+         */
     }
 }
