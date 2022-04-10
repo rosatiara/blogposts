0081f98bfb60910c1ae0081fc1760fa39220d2dd
rosa <rosatiaragaluh@gmail.com>
2022-04-10 15:21:58 +0700

    feat: add a column `name` to the author

--- database/migrations/2022_04_10_081800_create_authors_table.php
+++ database/migrations/2022_04_10_081800_create_authors_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('authors', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name');
         });
     }
 
