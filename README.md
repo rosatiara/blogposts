48a720fbf9756f4b8b171ee16d72fc3a4c1d17e7
rosa <rosatiaragaluh@gmail.com>
2022-04-13 14:13:06 +0700

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-blogposts
+# How-tos
+
+#### List of things that have helped me to solve errors for this project
+
+##### Migrate specific table
+
+```bash
+php artisan migrate --path=/database/migrations/2020_04_10_130703_create_test_table.php
+```
--- database/migrations/2022_04_10_092404_create_comments_table.php
+++ database/migrations/2022_04_10_092404_create_comments_table.php
@@ -13,11 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('blog_posts', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string('title');
-            $table->text('content');
         });
     }
 
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('blog_posts');
+        Schema::dropIfExists('comments');
     }
 };
