ba5b9f7ecc4a2dc9ad92225896775c2dd0a657b7
rosa <rosatiaragaluh@gmail.com>
2022-04-17 16:52:55 +0700

    style: modified blogposts page

--- resources/views/blogposts/index.blade.php
+++ resources/views/blogposts/index.blade.php
@@ -1,26 +1,57 @@
-<!-- blogposts.index.blade.php -->
 @extends('layout.app')
-@section('title', 'Blog Posts page')
+@section('title', 'Blog posts page')
 @section('content')
-  <h1>Our Blog Posts</h1>
-  <table class="table table-striped">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Title</th>
-          <th scope="col">Content</th>
-          <th scope="col">Highlight</th>
-        </tr>
-      </thead>
-      <tbody>
-        @foreach($blogposts as $key => $blogpost)
-        <tr>
-          <th scope="row">{{$key+1}}</th>
-          <td>{{$blogpost['blogPostTitle']}}</td>
-          <td>{{$blogpost['blogPostContent']}}</td>
-          <td>{{$blogpost['blogPostIsHighlight'] == 1 ? 'YES' : 'NO'}}</td>
-        </tr>
-        @endforeach
-      </tbody>
-  </table>  
+    <h1>Our Blog Posts</h1>
+    <table class="table table-stripped">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Title</th>
+                <th scope="col">Content</th>
+                <th scope="col">Highlight</th>
+                <th scope="col">All Comments</th>
+                <th scope="col">New Comments</th>
+                <th scope="col" colspan="2">
+                    @if (auth()->user() != null &&
+                            auth()->user()->hasRole(['Admin', 'Super-Admin']))
+                        Actions
+                    @endif
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($posts as $key => $post)
+                <tr>
+                    <th scope="row">{{ $key + 1 }}</th>
+                    <td>{{ $post['blogPostTitle'] }}</td>
+                    <td>{{ $post['blogPostContent'] }}</td>
+                    <td>{{ $post['blogPostIsHighlight'] == 1 ? 'YES' : 'NO' }}</td>
+                    <td>{{ $post['comments_count'] }}</td>
+                    <td>{{ $post['new_comments'] }}</td>
+                    <td>
+                        <a href="{{ route('blogposts.show', ['blogpost' => $post->id]) }}">
+                            <button class="btn btn-primary">View</button>
+                        </a>
+                    </td>
+                    <td>
+                        @can('edit Blog posts')
+                        <a href="{{ route('blogposts.edit', ['blogpost' => $post->id]) }}">
+                            <button class="btn btn-warning">Edit</button>
+                        </a>
+                        @endcan
+                    </td>
+                    <td>
+                        @if (auth()->user() != null &&
+                            auth()->user()->hasRole('Super-Admin'))
+                        <form action="{{ route('blogposts.destroy', ['blogpost' => $post->id]) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">Delete</button>
+                        </form>
+                        @endif
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
