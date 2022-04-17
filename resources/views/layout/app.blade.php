837798bc34fa607fb0aeb28375a36d5a0a5154be
rosa <rosatiaragaluh@gmail.com>
2022-04-17 16:51:46 +0700

    refactor: refactor some codes

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,25 +1,39 @@
 <!doctype html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link
-          rel="stylesheet"
-          href="{{asset('/css/app.css')}}"
-        >
-        <title>GIPE 2022 - @yield('title')</title>
-    </head>
-    <body>
-      <div class="container">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}"">
+    <title>GIPE 2022 - @yield('title')</title>
+</head>
+  @if (session('status'))
+    <div class="alert alert-success">
+        {{ session('status') }}
+    </div>
+    @endif
+
+<body>
+    @include('layout.partials.nav')
+    <div class='container'>
         @yield('content')
-      </div>
-      <footer>
-          <div class="text-center">
-            &copy; Global Intercultural Project experience <script>document.write(new Date().getFullYear())</script>
-          </div>
-      </footer>
-      <script
-      src="{{asset('js/app.js')}}"
-      ></script>
-    </body>
+    </div>
+    <footer>
+        <div class="text-center">
+            &copy; Global Intercultural Project Experience
+            <script>
+                document.write(new Date().getFullYear())
+            </script>
+            {{-- <script src="{{ asset('js/app.js') }}">
+            </script> --}}
+        </div>
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+
 </html>
