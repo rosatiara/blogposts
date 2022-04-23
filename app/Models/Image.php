45bb4e2437b71cf273bb63525c1535bc94615042
rosa <rosatiaragaluh@gmail.com>
2022-04-23 12:17:00 +0700

    feat: modified Image model

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
+use App\Models\BlogPost;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'imagePath', 'blog_post_id',
+    ];
+
+    public function blogPost(){
+        return $this->belongsTo(BlogPost::Class);
+    }
 }
