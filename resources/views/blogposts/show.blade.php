1a2cf6c34c831a62a49cfc5bd93715efe0e9e5da
rosa <rosatiaragaluh@gmail.com>
2022-04-23 12:37:59 +0700

    style: modified blogpost index page

--- resources/views/blogposts/show.blade.php
+++ resources/views/blogposts/show.blade.php
@@ -1,11 +1,6 @@
 @extends('layout.app')
 @section('title', 'Blog posts page')
 @section('content')
-    {{-- @if ($post['is_new'])
-    <div>This is a new blog post</div>
-        @elseif(!$post['is_new'])
-        <div>This is an old blog post</div>
-    @endif --}}
     <h1>{{$blogpost['blogPostTitle']}}</h1>
     <p>{{$blogpost['blogPostContent']}}</p>
 @endsection
