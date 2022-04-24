9249bd0d69fe9838c687406c611163acd65d90df
rosa <rosatiaragaluh@gmail.com>
2022-04-24 11:12:19 +0700

    fix: protected fillable for blogposts id

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -10,7 +10,7 @@ class Image extends Model
 {
     use HasFactory;
     protected $fillable = [
-        'imagePath', 'blog_post_id',
+        'imagePath', 'blog_posts_id',
     ];
 
     public function blogPost(){
