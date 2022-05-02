bef457b6f89cf152c8a556ca1dc80107ee20e392
rosa <rosatiaragaluh@gmail.com>
2022-05-02 21:41:16 +0700

    refactor: set $blogpost as public

--- app/Mail/BlogPostCreated.php
+++ app/Mail/BlogPostCreated.php
@@ -2,15 +2,19 @@
 
 namespace App\Mail;
 
+use App\Models\BlogPost;
+use App\Models\Comment;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
-use App\Models\BlogPost;
+
 
 class BlogPostCreated extends Mailable
 {
     use Queueable, SerializesModels;
+    
+    public $blogpost;
 
     /**
      * Create a new message instance.
